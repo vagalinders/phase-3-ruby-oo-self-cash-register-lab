c5f1d8c8db45eb34877893177d9bc9fb47d22695
Chris Mann <chrispmann@gmail.com>
2022-10-13 15:23:38 -0700

    commit tree dfe76cca155bd9eb3b450e1e1cd158ecf1435cb3

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,44 @@
+require "pry"
+
+class CashRegister
+    attr_reader :items
+    attr_accessor :discount, :total, :last_price
+
+
+    def initialize(discount = 0)
+        @discount = discount
+        @total = 0
+        @items = []
+        @last_price = 0
+    end
+
+    def total
+        @total
+    end
+    
+    
+
+    def add_item(title, price, quantity=1)
+        self.total += (quantity * price)
+        quantity.times do 
+            @items << title   
+        end
+        self.last_price = price * quantity
+    end
+
+    def apply_discount
+        if self.discount > 0
+            self.total -= (self.total * self.discount / 100) # (@total * (@discount / 100)) didn't work because 100 wasn't a float (needed 100.0)
+            "After the discount, the total comes to $#{@total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+
+    def void_last_transaction
+        self.total -= self.last_price
+    end
+
+end
 
